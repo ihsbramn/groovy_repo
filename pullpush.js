7e3369a7c14a7a8e81d448768e4dd8beba2b9d31
ihsbramn <abdurahmanihsan25@gmail.com>
2023-06-12 10:37:42 +0700

    add new files

---
+++ pullpush.js
@@ -0,0 +1,25 @@
+const exec = require('child_process').exec;
+
+exec('git add *',{cwd: './output/groovy'}, (err, stdout, stderr) => {  
+    if (err) {  
+      console.error(err);  
+      return;  
+    }  
+    console.log(stdout);  
+  });  
+
+exec('git commit -m "add new files"',{cwd: './output/groovy'}, (err, stdout, stderr) => {  
+  if (err) {  
+    console.error(err);  
+    return;  
+  }  
+  console.log(stdout);  
+});
+
+exec('git push',{cwd: './output/groovy'}, (err, stdout, stderr) => {  
+  if (err) {  
+    console.error(err);  
+    return;  
+  }  
+  console.log(stdout);  
+});
